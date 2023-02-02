0b27a53b80c17ce13e1f260f052f80e7aca5f548
Andrew Klepko <AYKlepko@rostatom.ru>
2023-02-02 18:46:58 +0300

    initial commit

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {FC, useMemo, useState} from "react";
+
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+const ThemeProvider: FC = ({children}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme)
+
+    const defaultProp = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProp}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
