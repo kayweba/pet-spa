66272651670a68fc058d59eb744df0302dd9c8b8
Andrew Klepko <AYKlepko@rostatom.ru>
2023-02-13 20:53:46 +0300

    Настроить storybook, добавить в него компоненты приложения

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentStory } from '@storybook/react';
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Sidebar } from 'widgets/Sidebar';
+
+export default {
+    title: 'widget/Sidebar',
+    component: Sidebar,
+    argTypes: {
+        backgroundColor: { control: 'black' },
+    },
+    decorators: [
+        ThemeDecorator(Theme.LIGHT),
+    ],
+};
+
+const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {};
+Light.decorators = [ThemeDecorator(Theme.LIGHT)];
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
