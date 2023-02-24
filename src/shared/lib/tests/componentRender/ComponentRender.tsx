1fe9456665ba8ec2ddfdc9868a160af0574512cd
Andrew Klepko <AYKlepko@rostatom.ru>
2023-02-24 17:04:55 +0300

    Добавить Redux Store, добавить первую сущность.

--- src/shared/lib/tests/componentRender/ComponentRender.tsx
+++ src/shared/lib/tests/componentRender/ComponentRender.tsx
@@ -2,23 +2,28 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
 import i18n from '../../../config/i18n/i18nForTests';
 
 export interface componentRenderOptions {
     route?: string
-
+    initialState?:DeepPartial<StateSchema>
 }
 
 export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
     const {
         route = '/',
+        initialState,
     } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18n}>
-                {component}
-            </I18nextProvider>
-        </MemoryRouter>,
+        <StoreProvider initialState={initialState as StateSchema}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18n}>
+                    {component}
+                </I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 }
