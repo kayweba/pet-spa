0a774e7ddd7ab7edbeca8c64d11437764757e709
Andrew Klepko <AYKlepko@rostatom.ru>
2023-03-12 17:16:45 +0300

    Реализовать запрос к api через thunk extra, добавить глобальную переменную __API__

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,7 +1,6 @@
 import { StoreProvider } from './ui/StoreProvider';
 import { createReduxStore, AppDispatch } from './config/store';
-import { ReduxStoreWithManager } from './config/StateSchema';
-import type { StateSchema } from './config/StateSchema';
+import type { StateSchema, ReduxStoreWithManager, ThunkConfig } from './config/StateSchema';
 
 export {
     StoreProvider,
@@ -9,4 +8,5 @@ export {
     StateSchema,
     ReduxStoreWithManager,
     AppDispatch,
+    ThunkConfig,
 };
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,30 +1,33 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { User, userActions } from 'entities/User';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
     username: string;
     password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, ThunkConfig<string>>(
     'users/loginByUsername',
     async (authData, thunkAPI) => {
+        const { extra, dispatch, rejectWithValue } = thunkAPI;
+
         try {
-            const response = await axios.post('http://localhost:8000/login', authData);
+            const response = await extra.api.post<User>('/login', authData);
 
             if (!response.data) {
                 throw new Error();
             }
 
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+            dispatch(userActions.setAuthData(response.data));
+            extra.navigate('/profile');
 
             return response.data;
         } catch (e) {
             console.log(e);
-            return thunkAPI.rejectWithValue('Вы ввели неверный логин или пароль');
+            return rejectWithValue('Вы ввели неверный логин или пароль');
         }
     },
 );
