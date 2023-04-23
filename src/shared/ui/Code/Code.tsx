6126c2b2b791843576272e68b6e0c6de4dee07ed
Andrew Klepko <AYKlepko@rostatom.ru>
2023-04-23 23:46:30 +0300

    Добавить сущность статьи, написать тесты, обновить сторибук, компонент скелетона

---
+++ src/shared/ui/Code/Code.tsx
@@ -0,0 +1,30 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo, ReactNode, useCallback } from 'react';
+import { Icon } from 'shared/ui/Icon/Icon';
+import CopyIcon from 'shared/assets/icons/copy.svg';
+import cls from './Code.module.scss';
+import { Button, ButtonTheme } from '../Button/Button';
+
+interface CodeProps {
+    className?: string;
+    text: string
+}
+
+export const Code = memo((props: CodeProps) => {
+    const { className, text } = props;
+
+    const onCopy = useCallback(async () => {
+        await navigator.clipboard.writeText(text);
+    }, [text]);
+
+    return (
+        <pre className={classNames(cls.Code, {}, [className])}>
+            <Button onClick={onCopy} className={cls.copyBtn} theme={ButtonTheme.CLEAR}>
+                <CopyIcon className={cls.copyIcon} />
+            </Button>
+            <code>
+                {text}
+            </code>
+        </pre>
+    );
+});
